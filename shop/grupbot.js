7e73d2b17571bcfaac93f42858f47772dc64daae
Zackyerzainh <95838168+Zackyerzainh@users.noreply.github.com>
2021-12-22 08:01:14 +0700

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -3,13 +3,9 @@ const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
 1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
+_https://chat.whatsapp.com/Ghy8sGWOWbeDq0sXDYn38p_
 2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+_https://chat.whatsapp.com/LsSFfq3wGr88vgE3la1Ihn_
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
